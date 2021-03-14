4b1d62d969dbe941902b59b1c2c4b222ae871ed6
Neeraj Singh Junior <NeerajSingh511997@gmail.com>
2021-03-14 21:56:00 +0530

    Added Migration, Controller, Factory, Model

---
+++ database/migrations/2021_03_14_111727_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('tag_id');
+            $table->unsignedInteger('category_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->longText('description');
+            $table->softDeletesTz();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
